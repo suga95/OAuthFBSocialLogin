a58fc48e4e2823cf2670de7d2c60b029ad2384b9
Suga95 <mahadevah.suganthan@gmail.com>
2018-10-13 11:44:13 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
-class App extends Component {
+
+import Facebook from './Component/Facebook';
+
+  class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">OAuth 2.0  Framework using FB</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This App Authenticates with Facebook
+            
+            <Facebook/>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
